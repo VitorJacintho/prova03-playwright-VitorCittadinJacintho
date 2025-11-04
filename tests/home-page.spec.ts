8fd7e20f16d242288cb69a2bff882b94936c2e48
Vitor Cittadin Jacintho <vitor.878120@alunosatc.edu.br>
2025-11-04 20:09:09 -0300

    tests playwright

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -5,14 +5,11 @@ test.describe('Anasac Home Page', () => {
   test('Validar título e elementos principais', async ({ page }) => {
     await page.goto('https://anasac.com/en/');
 
-    // Verifica o título
     await expect(page).toHaveTitle(/Anasac/i);
 
-    // Verifica se existe algum menu de navegação (resiliente)
     const nav = page.locator('nav, header, .elementor-nav-menu');
     await expect(nav.first()).toBeVisible();
 
-    // Verifica se o link "Contact" existe (em qualquer forma de "Contact us" ou "Contáctanos")
     const contactLink = page.locator('a:has-text("Contact")');
     await expect(contactLink.first()).toBeVisible();
   });
